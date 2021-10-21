9235cd929e2dc16f5e8f178fb17cfc9662260885
Moki <mokiong1427@gmail.com>
2021-10-21 14:15:51 +0800

    Extract url queries

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage() {
+    const router = useRouter();
+
+    console.log(router.pathname);
+
     return (
         <div>
             <h1>portfolio project page</h1>
