0d32ad9837a15aab6e4d93e4b8ad3f2e05b68688
Moki <mokiong1427@gmail.com>
2021-10-21 14:55:36 +0800

    Create Links for pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 function HomePage() {
     return (
         <div>
             <h1>The homepage</h1>
+            <ul>
+                <li>
+                    <Link href="/portfolio">Portfolio</Link>
+                </li>
+            </ul>
         </div>
     );
 }
