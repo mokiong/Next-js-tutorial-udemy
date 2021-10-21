465f195510d4bce0f49f70769ea5fdca7e49bb5e
Moki <mokiong1427@gmail.com>
2021-10-21 15:00:02 +0800

    Navigate to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 function ClientPage() {
     return (
         <div>
             <h1>The clients page</h1>
+            <ul>
+                <li>
+                    <Link href="/portfolio/max">Max</Link>
+                </li>
+                <li>
+                    <Link href="/portfolio/man">Man</Link>
+                </li>
+            </ul>
         </div>
     );
 }
