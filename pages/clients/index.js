6ec3b33ec66d57230fba918495e185a49ea0b047
Moki <mokiong1427@gmail.com>
2021-10-21 15:15:15 +0800

    Setup link destination

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,16 +1,32 @@
 import Link from 'next/link';
 
 function ClientPage() {
+    const clients = [
+        {
+            id: 'max',
+            name: 'Max',
+        },
+        {
+            id: 'man',
+            name: 'Man',
+        },
+    ];
+
     return (
         <div>
             <h1>The clients page</h1>
             <ul>
-                <li>
-                    <Link href="/portfolio/max">Max</Link>
-                </li>
-                <li>
-                    <Link href="/portfolio/man">Man</Link>
-                </li>
+                {clients.map((client) => (
+                    <Link
+                        href={{
+                            pathname: '/clients/[id]',
+                            query: { id: client.id },
+                        }}
+                        key={client.id}
+                    >
+                        {client.name}
+                    </Link>
+                ))}
             </ul>
         </div>
     );
